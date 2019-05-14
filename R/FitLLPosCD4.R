10c133280cd1d445f17df7d9070f204a736dec17
Daniel Lewandowski <daniel@nextpagesoft.net>
2019-05-14 19:42:10 +0200

    Initialize LL_PosCD4_Year value to 0 if not computed
    This is likely a bug in the C implementation.

--- R/FitLLPosCD4.R
+++ R/FitLLPosCD4.R
@@ -11,20 +11,24 @@ FitLLPosCD4 <- function(
   N_HIV_Stage <- sprintf('N_HIV_Stage_%d', group)
   LL_PosCD4_Year <- sprintf('LL_PosCD4_Year_%d', group)
 
-  # year <- 1
+  # year <- 29
   for (year in seq_len(nrow(modelResults))) {
     TotModel <- modelResults[year, get(N_HIV_Stage_S_Obs)]
     TotData <- data[year, get(N_HIV_Stage)]
 
-    if (TotModel > 0 &&
-        modelResults$Year[year] >= info$FitPosCD4MinYear &&
-        modelResults$Year[year] <= info$FitPosCD4MaxYear
+    if (
+      TotModel > 0 &&
+      modelResults$Year[year] >= info$FitPosCD4MinYear &&
+      modelResults$Year[year] <= info$FitPosCD4MaxYear
     ) {
       if (info$ModelFitDist == 1) {
         extraResults[year, (LL_PosCD4_Year) := FitLLPoisson(TotModel, TotData)]
       } else {
         stop('info$ModelFitDist != 1 not supported')
       }
+    } else {
+      extraResults[year, (LL_PosCD4_Year) := 0]
+      # message('Accessing old value: ', extraResults[year, get(LL_PosCD4_Year)])
     }
 
     L_PosCD4 <- L_PosCD4 + extraResults[year, get(LL_PosCD4_Year)]
