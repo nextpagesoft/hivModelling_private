0d3c4329b728df33f572f7d2fa14fe5c6131c7ee
Daniel Lewandowski <daniel@nextpagesoft.net>
2019-05-19 23:02:09 +0200

    Add deployment utilities

---
+++ utils/DEPLOY - 1. miniCRAN repo setup.R
@@ -0,0 +1,21 @@
+library(miniCRAN)
+
+repoCRAN <- 'https://cran.r-project.org/'
+
+pkgs <- c('data.table', 'future', 'xml2', 'Rcpp', 'utils', 'stats')
+pkgs <- setdiff(pkgs,
+                c('grid', 'graphics', 'parallel', 'stats', 'tools', 'utils'))
+
+pkgList <- pkgDep(pkgs,
+                  repos = repoCRAN,
+                  type = 'source',
+                  suggests = FALSE)
+
+repoPath <- 'd:/_DEPLOYMENT/hivModelling/repo'
+
+dir.create(repoPath, showWarnings = FALSE, recursive = TRUE)
+
+makeRepo(pkgList,
+         path = repoPath,
+         repos = repoCRAN,
+         type = c('source', 'win.binary'))
