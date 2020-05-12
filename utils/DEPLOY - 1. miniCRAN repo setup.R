0ae5de9d47d5c5fcc1e79bf196bfc1ea47f7b369
Daniel Lewandowski <daniel@nextpagesoft.net>
2020-05-12 08:57:08 +0200

    Update dependencies

--- utils/DEPLOY - 1. miniCRAN repo setup.R
+++ utils/DEPLOY - 1. miniCRAN repo setup.R
@@ -1,16 +1,18 @@
 library(miniCRAN)
 
 rVersion <- '4.0'
+repoPath <- 'd:/_DEPLOYMENT/hivModelling/repo'
 repoCRAN <- 'https://cran.r-project.org/'
 
 pkgs <- c(
   'cli', 'data.table', 'fs', 'future', 'ggplot2', 'nloptr', 'xml2', 'Rcpp', 'utils', 'stats'
 )
 pkgs <- setdiff(pkgs, c('grid', 'graphics', 'parallel', 'stats', 'tools', 'utils'))
-
 pkgList <- pkgDep(pkgs, repos = repoCRAN, type = 'source', suggests = FALSE)
 
-repoPath <- 'd:/_DEPLOYMENT/hivModelling/repo'
+if (dir.exists(repoPath)) {
+  unlink(repoPath, recursive = TRUE)
+}
 dir.create(repoPath, showWarnings = FALSE, recursive = TRUE)
 
 makeRepo(pkgList, path = repoPath, repos = repoCRAN, type = c('source', 'win.binary'))
