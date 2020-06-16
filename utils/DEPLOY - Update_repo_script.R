9e98f175b82e9b2cdb01a294a30acb0f785630e7
Daniel Lewandowski <daniel@nextpagesoft.net>
2020-06-16 21:32:22 +0200

    Remove scripts for repository creation
    This is handled by hivEstimatesAccuracy2 package

--- utils/DEPLOY - Update_repo_script.R
+++ utils/DEPLOY - Update_repo_script.R
@@ -6,6 +6,7 @@ devtools::document()
 
 # Build source and binary versions
 repoPath <- 'd:/_DEPLOYMENT/hivModelling/pkgBuilds'
+deployPath <- 'd:/_DEPLOYMENT/hivEstimatesAccuracy2'
 dir.create(repoPath, showWarnings = FALSE, recursive = TRUE)
 devtools::build(path = repoPath, binary = FALSE)
 devtools::build(path = repoPath, binary = TRUE, args = c('--preclean'))
@@ -20,24 +21,11 @@ zipFileName <- paste0('hivModelling_', version, '.zip')
 # Copy package files to appropriate subfolders
 file.copy(
   file.path(repoPath, tarFileName),
-  file.path(repoPath, '..', 'repo', 'src', 'contrib', tarFileName),
+  file.path(deployPath, 'repo', 'src', 'contrib', tarFileName),
   overwrite = TRUE
 )
 file.copy(
   file.path(repoPath, zipFileName),
-  file.path(repoPath, '..', 'repo', 'bin', 'windows', 'contrib', rVersion, zipFileName),
+  file.path(deployPath, 'repo', 'bin', 'windows', 'contrib', rVersion, zipFileName),
   overwrite = TRUE
 )
-
-# Update repo metafiles
-tools::write_PACKAGES(
-  dir = file.path(repoPath, '..', 'repo', 'src', 'contrib'),
-  type = 'source'
-)
-tools::write_PACKAGES(
-  dir = file.path(repoPath, '..', 'repo', 'bin', 'windows', 'contrib', rVersion),
-  type = 'win.binary'
-)
-
-# Update currect version string
-writeLines(version, file.path(repoPath, 'version.txt'))
